7d7bd439b4a8ff84c7d9be50309c8fb75c190a28
eric <eric.thomas@formation-industries-fc.fr>
2021-10-07 16:37:51 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int nblance,score=0;
+    printf("Saisir votre nombre de lancés");
+    scanf("%d",&nblance);
+    score=nblance*10;
+    /*****************************lancés***************************/
+    while(score>=0&&nblance>=0)
+    {
+        //TO DO lancement des 3 dés
+
+        //TO DO test 421
+
+        //TO DO relance des dés
+
+        //TO DO Gestion des scores
+
+        //TO DO Gestion de la fin de partie
+    }
+    return 0;
+}
