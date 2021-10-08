b4b79df3cacaa63f5fc5be2f228b5333aa614fa0
eric <eric.thomas@formation-industries-fc.fr>
2021-10-08 14:27:47 +0200

    TO DO 4 partiel relance

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 int main() {
-    int nblance = 0, score = 0, de1 = 0, de2 = 0, de3 = 0, saisie = 0, cptrelance = 0;
+    int nblance = 0, score = 0, de1 = 0, de2 = 0, de3 = 0, saisie = 0, cpt_relance = 0;
     char saisierelance;
 
     printf("Saisir votre nombre de lancés");
@@ -35,59 +35,62 @@ int main() {
         } else {
             printf("\nPerdu\n");
             /*****************************question relance du joueur*************************/
-            printf("Souhaitez-vous relancer? O/N");
-            scanf(" %c",&saisierelance);
-            /******************************relance*******************************************/
-            if(saisierelance=='O')
-            {
-                printf("**************RELANCE**************************\n");
-                printf("\nQuels sont les dés que vous voulez relancer\n");
-                printf("1-Les 3 dés\n");
-                printf("2- de1 et de2\n");
-                printf("3- de2 et de3\n");
-                printf("4- de1 et de3\n");
-                printf("5-uniquement de1\n");
-                printf("6- uniquement de2\n");
-                printf("7- uniquement de3\n");
-                scanf("%d",&saisie);
-                switch (saisie) {
+            if (cpt_relance < 2) {
+                printf("Souhaitez-vous relancer? O/N");
+                scanf(" %c", &saisierelance);
+                /******************************relance*******************************************/
+                if (saisierelance == 'O') {
+                    printf("**************RELANCE**************************\n");
+                    printf("\nQuels sont les dés que vous voulez relancer\n");
+                    printf("1- Les 3 dés\n");
+                    printf("2- de1 et de2\n");
+                    printf("3- de2 et de3\n");
+                    printf("4- de1 et de3\n");
+                    printf("5- uniquement de1\n");
+                    printf("6- uniquement de2\n");
+                    printf("7- uniquement de3\n");
+                    scanf("%d", &saisie);
+                    switch (saisie) {
 
-                    case 1:
-                        de1=0;
-                        de2=0;
-                        de3=0;
-                        break;
-                    case 2:
-                        de1=0;
-                        de2=0;
-                        break;
-                    case 3:
-                        de2=0;
-                        de3=0;
-                        break;
-                    case 4:
-                        de1=0;
-                        de3=0;
-                        break;
-                    case 5:
-                        de1=0;
-                        break;
-                    case 6:
-                        de2=0;
-                        break;
-                    case 7:
-                        de3=0;
-                        break;
+                        case 1:
+                            de1 = 0;
+                            de2 = 0;
+                            de3 = 0;
+                            break;
+                        case 2:
+                            de1 = 0;
+                            de2 = 0;
+                            break;
+                        case 3:
+                            de2 = 0;
+                            de3 = 0;
+                            break;
+                        case 4:
+                            de1 = 0;
+                            de3 = 0;
+                            break;
+                        case 5:
+                            de1 = 0;
+                            break;
+                        case 6:
+                            de2 = 0;
+                            break;
+                        case 7:
+                            de3 = 0;
+                            break;
+
+                    }
+                    cpt_relance++;
+                    goto relance;
 
                 }
-                goto relance;
+                /****************traitement du compteur_relance >=2*******************/
 
+                //TO DO Gestion des scores
+
+                //TO DO Gestion de la fin de partie
             }
-            //TO DO Gestion des scores
-
-            //TO DO Gestion de la fin de partie
         }
-
     }
     return 0;
 }
